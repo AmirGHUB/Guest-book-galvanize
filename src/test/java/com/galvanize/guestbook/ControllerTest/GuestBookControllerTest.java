d8d1d9aef79a9e9bad987fbec1f7cea37f004cad
Alaa Elsaka <alaa.elsaka@cognizant.com>
2021-02-09 14:04:00 -0600

    implement REST Docs

--- src/test/java/com/galvanize/guestbook/ControllerTest/GuestBookControllerTest.java
+++ src/test/java/com/galvanize/guestbook/ControllerTest/GuestBookControllerTest.java
@@ -5,10 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.guestbook.Model.GuestBook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.request.RequestDocumentation;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class GuestBookControllerTest {
 
     @Autowired
@@ -27,16 +36,29 @@ public class GuestBookControllerTest {
     @Test
     public void getAllEntries() throws Exception {
 
+        GuestBook book = new GuestBook("Jon", "nice");
+        mockMvc
+                .perform(post("/api/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(book)))
+                .andExpect(status().isCreated());
+
+
         mockMvc
                 .perform(get("/api/visitors"))
                 .andExpect(status().isOk())
-                .andReturn();
+                .andExpect(jsonPath("[0].name").value("Jon"))
+                .andExpect(jsonPath("[0].comment").value("nice"))
+                .andDo(document("GuestBooks", responseFields(
+                        fieldWithPath("[0].name").description("This is the name of the commenter"),
+                        fieldWithPath("[0].comment").description("This is the comment in the Guest Book")
+                )));
     }
 
     @Test
     public void addEntry() throws Exception {
 
-        GuestBook guest1 = new GuestBook("sam","very nice");
+        GuestBook guest1 = new GuestBook("Jon","good");
 
 
         mockMvc
@@ -44,8 +66,11 @@ public class GuestBookControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(guest1)))
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("name").value("sam"))
-                .andExpect(jsonPath("comment").value("very nice"))
-                .andReturn();
+                .andExpect(jsonPath("name").value("Jon"))
+                .andExpect(jsonPath("comment").value("good"))
+                .andDo(document("AddGuestBook", responseFields(
+                        fieldWithPath("name").description("This is the name of the commenter"),
+                        fieldWithPath("comment").description("This is the comment in the Guest Book")
+                )));
     }
 }
